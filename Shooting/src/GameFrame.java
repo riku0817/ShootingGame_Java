bd2871e0d72551873a680248eb4672c2103c8814
riku0817 <inbederacade@gmail.com>
2024-06-24 10:04:15 +0900

    リスト12.4

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -13,6 +13,7 @@ public class GameFrame extends MyFrame {
 			GameWorld.player.move();
 			movePlayerBullets();
 			moveEnemies();
+			checkPlayerAndEnemies();
 			sleep(0.03);
 		}
 	}
@@ -36,6 +37,17 @@ public class GameFrame extends MyFrame {
 			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+	}
+
+	public void checkPlayerAndEnemies() {
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+			Enemy e = GameWorld.enemies.get(i);
+			if (Math.abs(e.x - GameWorld.player.x) <= 30 &&
+					Math.abs(e.y - GameWorld.player.y) <= 30) {
+				System.out.println("やられた!");
+				GameWorld.player.y = -1000;
+			}
 		}
 	}
 }
